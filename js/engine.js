2d2d6ba998d30a3efc2fea6fd46b9f249c8ca987
FabDot-IO <fabios.ap93@hotmail.com>
2019-03-03 23:12:50 +0100

    Update engine.js

--- js/engine.js
+++ js/engine.js
@@ -46,19 +46,9 @@ $.ajax({
         
         var actualid = 1
 
-        //visjs è buggato devo impostare un nuovo zoom dopo il primo caricamento
-        var optionsNEW = {
-            min: '1916-01-01',
-            max: '1919-01-01',
-            maxHeight: "200px",
-            minHeight: "200px",
-            zoomMin: 1500000000, //1500000000 per il girono esatto 
-            zoomMax: 3000000000
-            };
-        timeline.setOptions(optionsNEW);
         timeline.setSelection(1, {focus: true})
 
-        
+
         $('#carouselTitle').on('slid.bs.carousel', function () {
             actualid= $( ".active" ).find( ".actualcard").attr('id')
             timeline.setSelection(actualid, {focus: true})
