9063b4389cf3b52f51406e496aa3595dee7d32d8
FabDot-IO <fabios.ap93@hotmail.com>
2019-03-03 22:43:22 +0100

    Update engine.js

--- js/engine.js
+++ js/engine.js
@@ -37,7 +37,6 @@ $.ajax({
         maxHeight: "200px",
         minHeight: "200px",
         zoomMin: 3000000000, //1500000000 per il girono esatto
-        zoomMax: 10
         };
 
         // Create a Timeline
