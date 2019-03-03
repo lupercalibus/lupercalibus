22aa2917927fb70b4d28ce59077c65d694e9679b
FabDot-IO <fabios.ap93@hotmail.com>
2019-03-03 18:09:32 +0100

    first step engine

---
+++ js/engine.js
@@ -0,0 +1,61 @@
+var data1 = (function () {
+    var json = null;
+    $.ajax({
+        'async': false,
+        'global': false,
+        'url': "json/Baracca.json",
+        'dataType': "json",
+        'success': function (data) {
+            data1 = data;
+        }
+    });
+    return data1;
+});
+
+
+
+
+$(document).ready(function(data1) {
+var map = L.map('map', {
+ maxZoom: 18,
+ minZoom: 12,
+});
+map.setView([44.4915, 11.3355], 12);
+var osm = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+ attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+data1.forEach(function(arrayItem) {
+    if (arrayItem["number"] == 1) {
+        $('.TitlesContainer').html('<div class="carousel-item active"> <div class="row"> <div class="col-md-3"></div> <div class="col-md-6 mx-1"> <div class="card alert-secondary" align="center"> <h1 align="center">'+ arrayItem["place"] + '</h1> </div> </div> <div class="col-md-3"></div> </div> </div>');
+    }
+    else {
+        $('.TitlesContainer').html('<div class="carousel-item"> <div class="row"> <div class="col-md-3"></div> <div class="col-md-6 mx-1"> <div class="card alert-secondary" align="center"> <h1 align="center">'+ arrayItem["place"] + '</h1> </div> </div> <div class="col-md-3"></div> </div> </div>');
+    }
+})
+
+var container = document.getElementById('visualization');
+
+// Create a DataSet (allows two way data-binding)
+var items = new vis.DataSet([
+{id: 1, content: 'item 1', start: '1916-04-20'},
+{id: 2, content: 'item 2', start: '1916-04-14'},
+{id: 3, content: 'item 3', start: '1916-04-18'},
+{id: 4, content: 'item 4', start: '1917-04-16'},
+{id: 5, content: 'item 5', start: '1917-04-25'},
+{id: 6, content: 'item 6', start: '1917-04-27'}
+]);
+
+// Configuration for the Timeline
+var options = {
+min: '1916-01-01',
+max: '1918-01-01',
+maxHeight: "200px",
+minHeight: "200px",
+zoomMin: '3000000000' //1500000000 per il girono esatto
+};
+
+// Create a Timeline
+var timeline = new vis.Timeline(container, items, options);
+
+})
