1969a6c4c0f3d621c5f96bfca9ebc442fddcf9a7
FabDot-IO <fabios.ap93@hotmail.com>
2019-03-03 19:17:06 +0100

    dates

--- js/engine.js
+++ js/engine.js
@@ -28,19 +28,12 @@ $.ajax({
         var container = document.getElementById('visualization');
 
         // Create a DataSet (allows two way data-binding)
-        var items = new vis.DataSet([
-        {id: 1, content: 'item 1', start: '12-04-1916'},
-        {id: 2, content: 'item 2', start: '1916-04-14'},
-        {id: 3, content: 'item 3', start: '1916-04-18'},
-        {id: 4, content: 'item 4', start: '1917-04-16'},
-        {id: 5, content: 'item 5', start: '1917-04-25'},
-        {id: 6, content: 'item 6', start: '1917-04-27'}
-        ]);
+        var items = new vis.DataSet(datesarray);
 
         // Configuration for the Timeline
         var options = {
         min: '1916-01-01',
-        max: '1918-01-01',
+        max: '1919-01-01',
         maxHeight: "200px",
         minHeight: "200px",
         zoomMin: 3000000000 //1500000000 per il girono esatto
