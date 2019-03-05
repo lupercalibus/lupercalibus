85ab58827c4a0054d3f9fd0ea96a6a30144ade2d
FabDot-IO <fabios.ap93@hotmail.com>
2019-03-05 11:47:51 +0100

    Update engine.js

--- js/engine.js
+++ js/engine.js
@@ -57,7 +57,7 @@ $.ajax({
         var options = {
         min: '1916-01-01',
         max: '1919-01-01',
-        maxHeight: "200px",
+        //maxHeight: "200px",
         minHeight: "200px",
         zoomMin: 1500000000, //1500000000 per il girono esatto
         zoomMax: 1500000000
