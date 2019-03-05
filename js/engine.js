957a2bd72ed359beb86e7f472e2d0c3355b74429
FabDot-IO <fabios.ap93@hotmail.com>
2019-03-05 14:44:18 +0100

    new css

--- js/engine.js
+++ js/engine.js
@@ -86,7 +86,6 @@ $.ajax({
         $('#carouselTitle').on('slid.bs.carousel', function () {
             actualid= $( ".active" ).find( ".actualcard").attr('id')
             timeline.setSelection(actualid, {focus: true})
-            timeline.h(actualid.title, actualid)
             //json del nuovo oggetto
             var actualitem
             actualitem = dati.find(function(dati){return dati.number ==  actualid})
