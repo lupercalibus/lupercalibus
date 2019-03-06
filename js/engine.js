4dd553aaf1c7015e84ece9b10a87d5d740b129dd
FabDot-IO <fabios.ap93@hotmail.com>
2019-03-06 23:57:00 +0100

    Update engine.js

--- js/engine.js
+++ js/engine.js
@@ -78,7 +78,8 @@ $.ajax({
         var layerGroup = L.layerGroup().addTo(map);
 
         var actualplace= L.geoJson(places.responseJSON, {filter: FirstPlaceFilter}).addTo(layerGroup);
-        map.setView(actualplace.getLatLng(),10);
+        var actualpoint = actualplace.getLatLng()
+        map.setView(actualpoint,10);
         function FirstPlaceFilter(feature) {
             if (feature.properties.name === firstitem["place"]) return true
             }
@@ -94,7 +95,8 @@ $.ajax({
 
             layerGroup.clearLayers();
             var actualplace= L.geoJson(places.responseJSON, {filter: PlaceFilter}).addTo(layerGroup);
-            map.setView(actualplace.getLatLng(),10);
+            var actualpoint = actualplace.getLatLng()
+            map.setView(actualpoint,10);
             function PlaceFilter(feature) {
                 if (feature.properties.name === actualitem["place"]) return true
                 }
