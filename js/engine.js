74973868252f7c10a8615a1483281b4782592ebc
FabDot-IO <fabios.ap93@hotmail.com>
2019-03-06 23:46:27 +0100

    Update engine.js

--- js/engine.js
+++ js/engine.js
@@ -6,9 +6,10 @@ var map = L.map('map', {
  minZoom: 10,
 });
 map.setView([46.0160, 13.1611], 10);
-var osm = L.tileLayer('https://tile.thunderforest.com/spinal-map/{z}/{x}/{y}.png', {
- attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+var osm = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 }).addTo(map);
+
 
 //'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 //per caricare il geojson serve un procedimento diverso
