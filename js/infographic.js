d171449da0235017a86f1ed06ae6a68c1566a35f
FabDot-IO <fabios.ap93@hotmail.com>
2019-03-14 14:58:28 +0100

    Update infographic.js

--- js/infographic.js
+++ js/infographic.js
@@ -1,5 +1,35 @@
 $(document).ready(function() {
-    var datitotsegn = {
+
+
+
+    var optionpie = {
+        responsiveAnimationDuration: 1000,
+        plugins: {
+            outlabels: {
+                display: false,
+            },
+            labels: [{
+                    render: 'label',
+                    position: 'outside',
+                    fontSize: 18,
+                    fontStyle: 'bold',
+                    fontColor: '#000'
+                },
+                {
+                    render: 'value',
+                    fontSize: 18,
+                    fontStyle: 'bold',
+                    fontColor: '#fff'
+                }
+            ]
+        },
+        legend: {
+            display: false
+        }
+    };
+
+//VITTORIE PER SQUADRIGLIA
+    var datsquad = {
         labels: [
             "Squadriglia 1°",
             "Squadriglia 70°",
@@ -23,7 +53,7 @@ $(document).ready(function() {
     new Chart(squadgraph, {
         type: 'doughnut',
         data: datsquad,
-        options: opt
+        options: optionpie
     });
 
 
