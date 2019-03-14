0e3b283625a37743e8ee94807a4b73daafe40090
FabDot-IO <fabios.ap93@hotmail.com>
2019-03-14 16:28:30 +0100

    amchart

--- js/infographic.js
+++ js/infographic.js
@@ -1,33 +1,64 @@
 $(document).ready(function() {
 
-
-
-    var chart = AmCharts.makeChart("squadgraph", {
-        "type": "pie",
-        "dataProvider": [{
-          "slice": "1° Squadriglia",
-          "value": 1
-        }, {
-            "slice": "70° Squadriglia",
-            "value": 7
-        },{
-            "slice": "91° Squadriglia",
-            "value": 26
-        }],
-        "valueField": "value",
-        "titleField": "slice",
-        "labelsEnabled": false,
-        "pullOutRadius": 0,
-        "innerRadius": 60,
-        "allLabels": [{
-          "text": "34%",
-          "color": "#f40000",
-          "size": 20,
-          "align": "center",
-          "y": "55%"
-        }]
-      });
-
+    am4core.useTheme(am4themes_animated);
+    // Themes end
+    
+    // Create chart instance
+    var chart = am4core.create("squadgraph", am4charts.PieChart);
+    
+    // Add and configure Series
+    var pieSeries = chart.series.push(new am4charts.PieSeries());
+    pieSeries.dataFields.value = "Victories";
+    pieSeries.dataFields.category = "Squadriglia";
+    
+    // Let's cut a hole in our Pie chart the size of 30% the radius
+    chart.innerRadius = am4core.percent(30);
+    
+    // Put a thick white border around each Slice
+    pieSeries.slices.template.stroke = am4core.color("#fff");
+    pieSeries.slices.template.strokeWidth = 2;
+    pieSeries.slices.template.strokeOpacity = 1;
+    pieSeries.slices.template
+      // change the cursor on hover to make it apparent the object can be interacted with
+      .cursorOverStyle = [
+        {
+          "property": "cursor",
+          "value": "pointer"
+        }
+      ];
+    
+    pieSeries.alignLabels = false;
+    pieSeries.labels.template.bent = true;
+    pieSeries.labels.template.radius = 3;
+    pieSeries.labels.template.padding(0,0,0,0);
+    
+    pieSeries.ticks.template.disabled = true;
+    
+    // Create a base filter effect (as if it's not there) for the hover to return to
+    var shadow = pieSeries.slices.template.filters.push(new am4core.DropShadowFilter);
+    shadow.opacity = 0;
+    
+    // Create hover state
+    var hoverState = pieSeries.slices.template.states.getKey("hover"); // normally we have to create the hover state, in this case it already exists
+    
+    // Slightly shift the shadow and make it more prominent on hover
+    var hoverShadow = hoverState.filters.push(new am4core.DropShadowFilter);
+    hoverShadow.opacity = 0.7;
+    hoverShadow.blur = 5;
+    
+    // Add a legend
+    chart.legend = new am4charts.Legend();
+    
+    chart.data = [{
+      "Squadriglia": "1° Squadriglia",
+      "Victories": 1
+    },{
+      "Squadriglia": "70° Squadriglia",
+      "Victories": 7
+    }, {
+      "Squadriglia": "91° Squadriglia",
+      "Victories": 26
+    }];
 
 
 })
