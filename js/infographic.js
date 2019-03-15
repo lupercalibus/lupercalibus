4dce96c606a67a3793df4cdfd21b40eb623590dd
FabDot-IO <fabios.ap93@hotmail.com>
2019-03-15 15:17:02 +0100

    Update infographic.js

--- js/infographic.js
+++ js/infographic.js
@@ -60,8 +60,8 @@ $(document).ready(function() {
 
 
                 //Set font settings to draw it correctly.
-                var centerX = ((chart.chartArea.left + chart.chartArea.right) / 2) - chart.innerRadius + sidePaddingCalculated;
-                var centerY = ((chart.chartArea.top + chart.chartArea.bottom) / 2) - chart.innerRadius + sidePaddingCalculated;
+                var centerX = ((chart.chartArea.left + chart.chartArea.right) / 2) - chart.innerRadius + (sidePaddingCalculated/2);
+                var centerY = ((chart.chartArea.top + chart.chartArea.bottom) / 2) - chart.innerRadius + (sidePaddingCalculated/2);
                 img.width = elementWidth
                 //img.height = elementHeight
                 //Draw text in center
