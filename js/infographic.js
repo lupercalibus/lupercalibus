49ad36d44018529dc44bc5b163cb5547c8d0146b
FabDot-IO <fabios.ap93@hotmail.com>
2019-03-17 12:03:08 +0100

    Update infographic.js

--- js/infographic.js
+++ js/infographic.js
@@ -252,7 +252,7 @@ $(document).ready(function() {
                 }
             },
             legend: {
-                display: false
+                display: true
             },
         };
         $('input[type=radio][name=planes]').change(function() {
