f32b6aef5c947f54447ac4b8bc3acdca48196026
FabDot-IO <fabios.ap93@hotmail.com>
2019-04-07 17:20:41 +0200

    Update engine.js

--- js/engine.js
+++ js/engine.js
@@ -86,6 +86,9 @@ $.ajax({
                 datesarray.push({id: arrayItem["number"], title: (arrayItem["place"]) +" "+ (arrayItem["date"]), start: arrayItem["date"], className: "dogfight"})
                 $('<div class="carousel-item"> <div class="row"> <div class="col-md-3"></div> <div class="col-md-6 mx-1"> <div class="card alert-secondary" align="center"> <h1 class="actualcard" id="'+ arrayItem["number"] + '" align="center"> Victory #'+ arrayItem["number"]+ '</h1><h2>'+  arrayItem["place"] + " <br> "+ (arrayItem["date"]) + '</h2> </div> </div> <div class="col-md-3"></div> </div> </div>').appendTo('.carousel-inner');
             }
+        datesarray.push({id: arrayItem["35"], title: 'Montello 1918-06-19', start: 1918-06-19 , className: "dogfight"})
+        $('<div class="carousel-item"> <div class="row"> <div class="col-md-3"></div> <div class="col-md-6 mx-1"> <div class="card alert-secondary" align="center"> <h1 class="actualcard" id="35" align="center"> Last Flight </h1><h2>Montello <br> 1918-06-19 </h2> </div> </div> <div class="col-md-3"></div> </div> </div>').appendTo('.carousel-inner');
+
         })
         $.when(battles).done(function() {
         var container = document.getElementById('visualization');
