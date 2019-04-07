d6b53a01938f5502c068d4945a1bff5c80b0e041
FabDot-IO <fabios.ap93@hotmail.com>
2019-04-07 12:29:39 +0200

    Update engine.js

--- js/engine.js
+++ js/engine.js
@@ -390,7 +390,6 @@ $.ajax({
 
           $('.mapnav').on('show.bs.tab', function(e){
             console-console.log("oooooo");
-            map.invalidateSize();
             var resizeEvent = new Event('resize');
             window.dispatchEvent(resizeEvent);
             map.setZoom(8);
